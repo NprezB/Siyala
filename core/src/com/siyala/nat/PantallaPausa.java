519058b29eedb613d286f97710563a90e94423c4
Dess <dess0@outlook.es>
2017-03-03 15:40:53 -0600

    clase pantallaPausa

--- core/src/com/siyala/nat/PantallaPausa.java
+++ core/src/com/siyala/nat/PantallaPausa.java
@@ -2,7 +2,6 @@ package com.siyala.nat;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -17,7 +16,7 @@ import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 /**
- * Created by Dess on 16/02/2017.
+ * Created by Dess on 03/03/2017.
  */
 
 public class PantallaPausa implements Screen {
@@ -25,113 +24,107 @@ public class PantallaPausa implements Screen {
     private static final float ALTO = 800;
     private final Siyala siyala;
 
-    //Vista y camara
+    //camara y vista
     private OrthographicCamera camara;
     private Viewport vista;
 
-    //Texturas
-    private Texture texturaFondo;
-    private Texture botonMenu;
-    private Texture botonPlay;
-    private Texture botonSets;
+    //Texturas botones
+    private Texture texturaFondoPausa;
+    private Texture texturaBotonMenu;
+    private Texture texturaBotonPlay;
+    private Texture texturaBotonSetts;
 
-    //Para escenas
-    private SpriteBatch batch;
+    //Escenas
     private Stage escena;
+    private SpriteBatch batch;
 
-    public PantallaPausa(Siyala siyala){
+    public PantallaPausa(Siyala siyala) {
         this.siyala=siyala;
     }
+
 
     @Override
     public void show() {
         crearCamara();
         cargarTexturas();
         crearObjetos();
+
     }
 
     private void crearObjetos() {
         batch = new SpriteBatch();
         escena = new Stage(vista,batch);
-        Image imgFondo = new Image(texturaFondo);
+        Image imgFondo = new Image(texturaFondoPausa);
         escena.addActor(imgFondo);
 
-        //Botones
-        TextureRegionDrawable trdBtnPlay= new TextureRegionDrawable(new TextureRegion(botonPlay));
-        ImageButton btnPlay= new ImageButton(trdBtnPlay);
+
+        //Boton Continuar
+        TextureRegionDrawable trdBtnPlay = new TextureRegionDrawable(new TextureRegion(texturaBotonPlay));
+        ImageButton btnPlay = new ImageButton(trdBtnPlay);
         btnPlay.setPosition(ANCHO/2-btnPlay.getWidth()/2,6*ALTO/8);
         escena.addActor(btnPlay);
 
+        //Evento del Boton
         btnPlay.addListener(new ClickListener(){
-            public void clicked(InputEvent event, float x, float y){
-                siyala.setScreen(new PantallaMenu(siyala));
+            public void clicked(InputEvent event,float x, float y){
+                siyala.setScreen(new PantallaPlayHist(siyala));
             }
 
         });
 
-        TextureRegionDrawable trdBtnMenu=new TextureRegionDrawable(new TextureRegion(botonMenu));
-        ImageButton btnMenu= new ImageButton(trdBtnMenu);
-        btnMenu.setPosition(ANCHO/2-btnMenu.getWidth()/2,3*ALTO/4-btnMenu.getHeight()/2);
+        //Boton Menu
+        TextureRegionDrawable trdBtnMenu = new TextureRegionDrawable(new TextureRegion(texturaBotonMenu));
+        ImageButton btnMenu = new ImageButton(trdBtnMenu);
+        btnMenu.setPosition(ANCHO/2-btnMenu.getWidth()/2,4.5f*ALTO/8);
         escena.addActor(btnMenu);
 
-
+        //Evento del Boton Menu
         btnMenu.addListener(new ClickListener(){
-            public void clicked(InputEvent event, float x, float y){
+            public void clicked(InputEvent event,float x, float y){
                 siyala.setScreen(new PantallaMenu(siyala));
             }
 
         });
 
+        //Boton Settings
+        TextureRegionDrawable trdBtnSettings = new TextureRegionDrawable(new TextureRegion(texturaBotonSetts));
+        ImageButton btnSettings = new ImageButton(trdBtnSettings);
+        btnSettings.setPosition(ANCHO/2-btnSettings.getWidth()/2,3*ALTO/8);
+        escena.addActor(btnSettings);
 
-        TextureRegionDrawable trdBtnSettings=new TextureRegionDrawable(new TextureRegion(botonSets));
-        ImageButton btnSettings=new ImageButton(trdBtnSettings);
-        btnSettings.setPosition(ANCHO/2-btnSettings.getWidth()/2,4.5f*ALTO/8);
-
+        //Evento del Boton Settings
         btnSettings.addListener(new ClickListener(){
             public void clicked(InputEvent event, float x, float y){
                 siyala.setScreen(new PantallaSettings(siyala));
             }
-
         });
 
 
-
-
-
         Gdx.input.setInputProcessor(escena);
-
-
     }
 
     private void cargarTexturas() {
-        texturaFondo= new Texture("FondoPausa.png");
-        botonMenu= new Texture("BotonMenu.png");
-        botonPlay=new Texture("ContinueBoton.png");
-        botonSets=new Texture("BotonSettings.png");
+        texturaFondoPausa = new Texture("FondoPausa.png");
+        texturaBotonPlay = new Texture("ContinueBoton.png");
+        texturaBotonSetts = new Texture("BotonSettings.png");
+        texturaBotonMenu = new Texture("ExitBoton.png");
     }
 
     private void crearCamara() {
-        camara= new OrthographicCamera(ANCHO,ALTO);
+        camara = new OrthographicCamera(ANCHO, ALTO);
         camara.position.set(ANCHO/2,ALTO/2,0);
         camara.update();
-        vista=new StretchViewport(ANCHO,ALTO,camara);
+        vista = new StretchViewport(ANCHO,ALTO,camara);
     }
 
     @Override
     public void render(float delta) {
-        borrarPantalla();
-        escena.draw();
 
-    }
-
-    private void borrarPantalla() {
-        Gdx.gl.glClearColor(0,1,0,1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     }
 
     @Override
     public void resize(int width, int height) {
-        vista.update(width,height);
+
     }
 
     @Override
@@ -152,8 +145,10 @@ public class PantallaPausa implements Screen {
     @Override
     public void dispose() {
         escena.dispose();
-        botonMenu.dispose();
-        botonPlay.dispose();
-        botonSets.dispose();
+        texturaBotonMenu.dispose();
+        texturaBotonPlay.dispose();
+        texturaBotonSetts.dispose();
+        texturaFondoPausa.dispose();
+
     }
 }
