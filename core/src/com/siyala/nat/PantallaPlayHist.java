a90a6167dd9f9c3d232559f28a291442a3e9df7f
Ricardo <A01274369@itesm.mx>
2017-03-22 17:04:44 -0600

    Implementacion de Botón Provisional para cambio de mundos, recursos del mundo oscuro y codigo para hacer cambio de mundo

--- core/src/com/siyala/nat/PantallaPlayHist.java
+++ core/src/com/siyala/nat/PantallaPlayHist.java
@@ -44,12 +44,20 @@ public class PantallaPlayHist extends Pantalla {
     private float posiCamara = ANCHO/2;
 
     private TiledMap mapa;
+    private TiledMap mapaMundoOsc;
+    private OrthogonalTiledMapRenderer renderMapaMundoOsc;
     private OrthogonalTiledMapRenderer renderarMapa;
     private SpriteBatch batch;
 
     // Siyala
     private Personaje siyala;
     private Texture texturaSiyala;
+
+    //Cambio de mundo
+    private Texture botonSwitch;
+    private boolean estaenMundoVivo=false;
+    private float SwitchCooldownTime=0;
+    private float TiempoSwitch=0;
 
     // Música
     private Music musicaFondo;  // Sonidos largos
@@ -72,10 +80,18 @@ public class PantallaPlayHist extends Pantalla {
 
     @Override
     public void show() {
+
+        //BotonSwitch
+        botonSwitch= new Texture("BotonWorld.png");
+
         cargarRecursosSiyala();
         texturaSiyala = manager.get("siyala.png");
         siyala = new Personaje(texturaSiyala,182,14*32);
         cargarMapa();
+
+        crearHUD();
+        Gdx.input.setInputProcessor(escenaHUD);
+
         texto = new Texto("fuente.fnt");
 
         Gdx.input.setInputProcessor(new ProcesadorEntrada());
@@ -83,7 +99,39 @@ public class PantallaPlayHist extends Pantalla {
 
     }
 
+    private void crearHUD() {
+        // Cámara HUD
+        camaraHUD = new OrthographicCamera(ANCHO, ALTO);
+        camaraHUD.position.set(ANCHO / 2, ALTO / 2, 0);
+        camaraHUD.update();
+        vistaHUD = new StretchViewport(ANCHO, ALTO, camaraHUD);
+
+        // HUD
+        TextureRegionDrawable tr=new TextureRegionDrawable(new TextureRegion(botonSwitch));
+        ImageButton btn=new ImageButton(tr);
+
+        btn.addListener(new ClickListener(){
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                if ((!estaenMundoVivo && SwitchCooldownTime <= 0) || estaenMundoVivo) {
+                    cambiarMundo();
+                }
+                return true;
+            }
+        });
+
+
+
+        escenaHUD = new Stage(vistaHUD);
+        escenaHUD.addActor(btn);
+    }
+
+    private void cambiarMundo() {
+        estaenMundoVivo=!estaenMundoVivo;
+    }
+
     private void cargarRecursosSiyala() {
+        manager.load("Primer nivelosc.tmx",TiledMap.class);
         manager.load("siyala.png", Texture.class);
         manager.load("Primer nivel.tmx", TiledMap.class);
         manager.load("DarkMusic.mp3", Music.class);
@@ -91,6 +139,7 @@ public class PantallaPlayHist extends Pantalla {
     }
 
     private void cargarMapa() {
+        mapaMundoOsc = manager.get("Primer nivelosc.tmx");
         mapa = manager.get("Primer nivel.tmx");
         musicaFondo = manager.get("DarkMusic.mp3");
         musicaFondo.setLooping(true);
@@ -98,24 +147,57 @@ public class PantallaPlayHist extends Pantalla {
 
         batch = new SpriteBatch();
 
+        renderMapaMundoOsc = new OrthogonalTiledMapRenderer(mapaMundoOsc,batch);
         renderarMapa = new OrthogonalTiledMapRenderer(mapa, batch);
         renderarMapa.setView(camara);
+        renderMapaMundoOsc.setView(camara);
+
     }
 
     @Override
     public void render(float delta) {
         boolean pierde = false;
-        pierde = siyala.actualizar(mapa,delta,velociCamara);
+        pierde = siyala.actualizar(mapaMundoOsc,delta,velociCamara);
         //actualizarCamara();
         //posiCamara+=delta*velociCamara;
 
         borrarPantalla();
         batch.setProjectionMatrix(camara.combined);
-        renderarMapa.setView(camara);
-        renderarMapa.render();
+        //renderarMapa.setView(camara);
+       // renderarMapa.render();
+
+        //Mapa dependiendo del estado
+        if(!estaenMundoVivo) {
+          //  pierde = siyala.actualizar(mapaMundoOsc,delta,velociCamara);
+            siyala.actualizar(mapaMundoOsc, delta, velociCamara);
+            renderMapaMundoOsc.setView(camara);
+            renderMapaMundoOsc.render();
+        }
+
+        else if(estaenMundoVivo)
+        {
+           // pierde = siyala.actualizar(mapa,delta,velociCamara);
+            siyala.actualizar(mapa,delta,velociCamara);
+            renderarMapa.setView(camara);
+            renderarMapa.render();
+        }
 
         batch.begin();
         siyala.dibujar(batch);
+
+        if (estaenMundoVivo) {
+            TiempoSwitch += Gdx.graphics.getDeltaTime();
+            SwitchCooldownTime = TiempoSwitch;
+
+            if (TiempoSwitch >= 3) {
+                TiempoSwitch = 0;
+                estaenMundoVivo = !estaenMundoVivo;
+            }
+        }
+
+        if(!estaenMundoVivo && SwitchCooldownTime>=0)
+            SwitchCooldownTime-=Gdx.graphics.getDeltaTime();
+
         actualizarCamara();
         int distImprimir = ((int) distRecorrida);
         if(siyala.getEstadoMovimiento()!=Personaje.EstadoMovimiento.PERDIENDO){
@@ -127,6 +209,10 @@ public class PantallaPlayHist extends Pantalla {
             texto.mostrarMensaje(batch,"SCORE: " + distImprimir,camara.position.x,camara.position.y+140);
         }
         batch.end();
+
+        //Dibujar HUD
+        batch.setProjectionMatrix(camaraHUD.combined);
+        escenaHUD.draw();
 
         if (Gdx.input.isKeyJustPressed(Input.Keys.BACK)) {
             juego.setScreen(new PantallaMenu(juego));
@@ -166,6 +252,7 @@ public class PantallaPlayHist extends Pantalla {
         manager.unload("siyala.png");
         manager.unload("Primer nivel.tmx");
         manager.unload("DarkMusic.mp3");
+        manager.unload("Primer nivelosc.tmx");
     }
 
     private class ProcesadorEntrada implements InputProcessor
@@ -188,8 +275,11 @@ public class PantallaPlayHist extends Pantalla {
 
         @Override
         public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+
             v.set(screenX,screenY,0);
             camara.unproject(v);
+
+           // if(screenX>75 && screenY>75)
             if(siyala.getEstadoMovimiento()!= Personaje.EstadoMovimiento.PERDIENDO) {
                 if (!siyala.getDoubleJump()) {
                     if (siyala.getEstadoMovimiento() == Personaje.EstadoMovimiento.MOV_DERECHA && v.x > posiCamara) {
@@ -209,6 +299,11 @@ public class PantallaPlayHist extends Pantalla {
                     }
                 }
             }
+
+           /* else if(screenX<=75 && screenY<=75)
+                if ((!estaenMundoVivo && SwitchCooldownTime <= 0) || estaenMundoVivo)
+                    cambiarMundo();*/
+
             else{
                 juego.setScreen(new PantallaMenu(juego));
             }
